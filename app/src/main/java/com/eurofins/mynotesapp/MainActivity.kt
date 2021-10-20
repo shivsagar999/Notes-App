6fe59a437864b03e499770b3a47f57b50737c15d
Raghav Lalvani <F2WZ@eurofins.com>
2021-10-20 20:44:52 +0530

    Added Toolbar and Hamburger Icon

--- app/src/main/java/com/eurofins/mynotesapp/MainActivity.kt
+++ app/src/main/java/com/eurofins/mynotesapp/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.eurofins.mynotesapp
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
+        //setUpViews()
+    }
+
+    fun setUpViews(){
+        setUpDrawerLayout()
+    }
+
+    fun setUpDrawerLayout(){
+        setSupportActionBar(findViewById(R.id.appBar))
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, findViewById(R.id.drawer_layout),R.string.app_name,R.string.app_name)
+        actionBarDrawerToggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
