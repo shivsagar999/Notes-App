b870af9ab3c68d8d361cc4443e21defda34b1892
Raghav Lalvani <F2WZ@eurofins.com>
2021-10-21 14:31:00 +0530

    Added icons for delete on toolbar

--- app/src/main/java/com/eurofins/mynotesapp/HomeFragment.kt
+++ app/src/main/java/com/eurofins/mynotesapp/HomeFragment.kt
@@ -1,10 +1,9 @@
 package com.eurofins.mynotesapp
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.SearchView
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.coroutineScope
