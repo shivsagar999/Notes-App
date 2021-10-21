42dfa73cb5d126c0f1927abbc074888866b3e70b
Raghav Lalvani <F2WZ@eurofins.com>
2021-10-21 10:42:41 +0530

    Fixed the dissapearing of Hamburger Icon

--- app/src/main/java/com/eurofins/mynotesapp/CreateNoteFragment.kt
+++ app/src/main/java/com/eurofins/mynotesapp/CreateNoteFragment.kt
@@ -4,21 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.eurofins.mynotesapp.databinding.FragmentCreateNoteBinding
 
+
 class CreateNoteFragment : Fragment() {
 
     private lateinit var binding: FragmentCreateNoteBinding
 
     private val createNoteFragmentViewModel: NoteViewModel by activityViewModels {
-       NoteViewModelFactory( (activity?.application as NoteApplication).database.getNotesDao())
+        NoteViewModelFactory((activity?.application as NoteApplication).database.getNotesDao())
     }
 
-    private var id: Int ?= null
+    private var id: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,25 +51,40 @@ class CreateNoteFragment : Fragment() {
         }
 
         binding.saveButton.setOnClickListener {
-        if(id != 0){
-        createNoteFragmentViewModel.updateNote(binding.title.text.toString(),
-            binding.description.text.toString())
-            findNavController().navigate(R.id.action_createNoteFragment_to_homeFragment)
-        }else{
-            if(binding.title.text.toString().isNotEmpty() or
-                    binding.description.text.toString().isNotEmpty()){
-                val newNote = Note(noteTitle = binding.title.text.toString(),
-                                    noteDescription = binding.description.text.toString(),
-                                    timeStamp = "SSS")
-                createNoteFragmentViewModel.addNote(newNote)
+            if (id != 0) {
+                createNoteFragmentViewModel.updateNote(
+                    binding.title.text.toString(),
+                    binding.description.text.toString()
+                )
                 findNavController().navigate(R.id.action_createNoteFragment_to_homeFragment)
+            } else {
+                if (binding.title.text.toString().isNotEmpty() or
+                    binding.description.text.toString().isNotEmpty()
+                ) {
+                    val newNote = Note(
+                        noteTitle = binding.title.text.toString(),
+                        noteDescription = binding.description.text.toString(),
+                        timeStamp = "SSS"
+                    )
+                    createNoteFragmentViewModel.addNote(newNote)
+                    findNavController().navigate(R.id.action_createNoteFragment_to_homeFragment)
+                }
             }
-        }
 
 
         }
         binding.backButton.setOnClickListener {
             findNavController().navigate(R.id.action_createNoteFragment_to_homeFragment)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
     }
 }
