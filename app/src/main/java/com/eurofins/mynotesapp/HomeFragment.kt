b917d2ff642f7696d3ea8c74a823d9ef845bdf7a
Shivsagar S <Shivsagar935@gmail.com>
2021-10-25 22:29:17 +0530

    Added the Selected count to the menu

--- app/src/main/java/com/eurofins/mynotesapp/HomeFragment.kt
+++ app/src/main/java/com/eurofins/mynotesapp/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.SearchView
-import androidx.core.app.ActivityCompat.invalidateOptionsMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.coroutineScope
@@ -18,6 +17,7 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import java.util.*
 import kotlin.collections.ArrayList
+
 
 class HomeFragment : Fragment() {
 
